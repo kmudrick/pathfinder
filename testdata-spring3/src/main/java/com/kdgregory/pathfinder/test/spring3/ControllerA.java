c30b8d095581921c91f2fc49c712b213721b9ab7
Keith Gregory <kgregory@chariotsolutions.com>
2012-05-30 08:06:28 -0400

    Spring 3 test war - initial version

---
+++ testdata-spring3/src/main/java/com/kdgregory/pathfinder/test/spring3/ControllerA.java
@@ -0,0 +1,45 @@
+// Copyright (c) Keith D Gregory
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.kdgregory.pathfinder.test.spring3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+
+@Controller
+public class ControllerA
+{
+    @RequestMapping(value="/foo.html", method=RequestMethod.GET)
+    protected ModelAndView basicGet(
+            HttpServletRequest request,
+            HttpServletResponse response)
+    throws Exception
+    {
+        Map<String,String> model = new HashMap<String,String>();
+        model.put("reqUrl", request.getRequestURI());
+        model.put("controller", getClass().getName());
+        return new ModelAndView("simple", "data", model);
+    }
+
+}
